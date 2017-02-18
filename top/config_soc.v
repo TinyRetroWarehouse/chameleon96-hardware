89b9cca2cdfdd6af6ebc3b60073c4b8e4b195a69
Dan Negvesky <dnegvesky@arrow.com>
2017-02-17 22:37:32 -0500

    initial commit - 1920x1080; 148.5MHz pixel and video clock; 256 bit MM master interface

---
+++ top/config_soc.v
@@ -0,0 +1,7 @@
+// Comment out any `define's below to remove top level interfaces/components that are not required
+// Setting 'define to false does not remove the interface/component from the top level
+
+`define hdmi true
+`define wifi_bt true
+//`define connector_30x2 true
+`define connector_20x2 true
